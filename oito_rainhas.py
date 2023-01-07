48c41260daedc8a0ad375625f667a9c0343c75a2
pafev <paulofvilarimda@gmail.com>
2023-01-07 15:17:52 -0300

    estrutura base do código

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -1,3 +1,7 @@
+valid = 1
+rainhas_coordenadas = []
+count = 0
+
 def checaAtaqueHorizontal():
     pass
 
@@ -9,3 +13,23 @@ def checaAtaqueDiagonal():
 
 def checaEntrada():
     pass
+
+def recebeDados(linha, posicao_linha, rainhas_coordenadas):
+    pass
+
+
+while count < 8 and valid == 1:
+    valid = recebeDados(input(), count, rainhas_coordenadas)
+    count += 1
+
+if valid == 1:
+    valid = checaEntrada(rainhas_coordenadas)
+elif valid == 1:
+    valid = checaAtaqueHorizontal(rainhas_coordenadas)
+elif valid == 1:
+    valid = checaAtaqueVertical(rainhas_coordenadas)
+elif valid == 1:
+    valid = checaAtaqueDiagonal(rainhas_coordenadas)
+
+
+print(valid)
