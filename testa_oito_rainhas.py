370f1e30a3f5683b104e6e0e627d2f7492c92d42
pafev <paulofvilarimda@gmail.com>
2023-01-07 14:58:52 -0300

    testes ataque diagonal e processamento da entrada

--- testa_oito_rainhas.py
+++ testa_oito_rainhas.py
@@ -1,4 +1,4 @@
-from oito_rainhas import checaAtaqueHorizontal, checaAtaqueVertical, checaAtaqueDiagonal, checaEntrada
+from oito_rainhas import checaAtaqueHorizontal, checaAtaqueVertical, checaAtaqueDiagonal, checaEntrada, recebeDados
 
 def testa_checaAtaqueHorizontal():
     tabuleiroDados1 = [(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (0,6), (0,7)]
@@ -14,12 +14,42 @@ def testa_checaAtaqueVertical():
     tabuleiroDados2 = [(1,0), (5,0), (7,1), (3,3), (0,4), (2,5), (4,6), (6,7)]
     tabuleiroDados3 = [(0,0), (6,1), (3,2), (5,3), (7,4), (1,5), (4,6), (2,7)]
 
-    assert checaAtaqueHorizontal(tabuleiroDados1) == 0
-    assert checaAtaqueHorizontal(tabuleiroDados2) == 0
-    assert checaAtaqueHorizontal(tabuleiroDados3) == 1
+    assert checaAtaqueVertical(tabuleiroDados1) == 0
+    assert checaAtaqueVertical(tabuleiroDados2) == 0
+    assert checaAtaqueVertical(tabuleiroDados3) == 1
 
 def testa_checaAtaqueDiagonal():
-    pass
+    tabuleiroDados1 = [(0,0), (1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7)]
+    tabuleiroDados2 = [(0,6), (1,0), (2,2), (3,5), (4,7), (5,1), (6,3), (7,5)]
+    tabuleiroDados3 = [(0,4), (1,1), (2,3), (3,6), (4,2), (5,7), (6,5), (7,0)]
+
+    assert checaAtaqueDiagonal(tabuleiroDados1) == 0
+    assert checaAtaqueDiagonal(tabuleiroDados2) == 0
+    assert checaAtaqueDiagonal(tabuleiroDados3) == 1
+
+
+def testa_recebeDados():
+    valid = None
+    rainhas_coordenadas = []
+    linha1 = '00000000'
+    linha2 = '00100010'
+    linha3 = '000011000'
+    linha4 = '0000000'
+
+    valid = recebeDados(linha1, 0, rainhas_coordenadas)
+    assert valid == 1
+    assert rainhas_coordenadas == []
+
+    valid = recebeDados(linha2, 1, rainhas_coordenadas)
+    assert valid == 1
+    assert (1, 2) in rainhas_coordenadas and (1, 6) in rainhas_coordenadas
+
+    valid = recebeDados(linha3, 2, rainhas_coordenadas)
+    assert valid == -1
+
+    valid = recebeDados(linha4, 3, rainhas_coordenadas)
+    assert valid == -1
+    
 
 def testa_checaEntrada():
     pass
